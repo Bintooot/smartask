5848370c4de67bf12d5b83b99583ba84c8fa3b87
RamDiv <142648607+Bintooot@users.noreply.github.com>
2025-06-25 19:45:09 +0800

    adding animation for navlinks

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 import "./Header.css";
+import { Github, CircleUser } from "lucide-react";
 
-const navlink = ["Home", "Overview", "About", "Contact"];
+const navlink = [
+  {
+    id: "1",
+    label: "Home",
+    href: "#",
+    title: "home",
+  },
+  {
+    id: "2",
+    label: "Overview",
+    href: "#",
+    title: "overview",
+  },
+  {
+    id: "3",
+    label: "About",
+    href: "#",
+    title: "about",
+  },
+  {
+    id: "4",
+    label: "Contact",
+    href: "#",
+    title: "contact",
+  },
+];
 
 const Header = () => {
   return (
@@ -12,21 +38,31 @@ const Header = () => {
           <nav className="header-navlink">
             <ul>
               {navlink.map((items) => (
-                <li>
-                  <a href="#">{items}</a>
+                <li key={items.id}>
+                  <a href={items.href} title={items.title}>
+                    {items.label}
+                  </a>
                 </li>
               ))}
             </ul>
           </nav>
         </div>
         <div className="nav-link-wrapper">
-          <a href="#">Github</a>
           <a
+            title="github repository"
+            href="https://github.com/Bintooot/smartask"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Github size={20} />
+          </a>
+          <a
+            title="developer"
             href="https://aniascoben-portfolio.vercel.app/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Portfolio
+            <CircleUser size={20} />
           </a>
         </div>
       </div>
